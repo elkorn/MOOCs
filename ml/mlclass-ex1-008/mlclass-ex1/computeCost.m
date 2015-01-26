059d08a8b95474df22785ec0d55ae108ae0c5308
Korneliusz Caputa <helluinster@gmail.com>
2015-01-26 19:45:31 +0100

    [ML|ex1] Solve 1 and 2.

--- ml/mlclass-ex1-008/mlclass-ex1/computeCost.m
+++ ml/mlclass-ex1-008/mlclass-ex1/computeCost.m
@@ -12,11 +12,14 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+function out = h(x, theta)
+    out = theta(1)*x(1)  + theta(2)*x(2);
+end
 
+for i=1:m
+    J = J + (h(X(i, :), theta)-y(i))^2;
+end
 
-
-
-
+J = J / (2.0*m);
 % =========================================================================
-
 end
